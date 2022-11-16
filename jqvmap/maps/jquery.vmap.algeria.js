11d678eb1653f2ba323448ff300584dc6dfae996
TA-TKU <160610@gms.tku.edu.tw>
2022-11-16 19:41:48 +0800

    Analysis on reviews of restaurants in Taiwan on Google Maps

---
+++ jqvmap/maps/jquery.vmap.algeria.js
@@ -0,0 +1,2 @@
+/** Add Algeria Map Data Points */ 
+jQuery.fn.vectorMap('addMap', 'dz_fr', {"pathes": {"1": {"path":M724.01,226.68l-0.74,1.48l-0.9,-1.52l-0.25,-1.74l1.38,-2.44l1.73,-1.74l0.64,0.44l-1.85,5.52Z}}});
